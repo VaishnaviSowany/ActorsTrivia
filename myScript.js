c55c3b377647131a204e8abf5ced68386f627f52
VaishnaviSowany <vaishnavisowany@gmail.com>
2017-09-06 21:56:36 -0400

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,13 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+   $("#abcd").hide();
+    $("#demo").click(function(){
+        $("#abcd").toggle();
+    });
+});
+
+  function Redirect() {
+               window.location="arkadium3.html";
+            }
+</script>
